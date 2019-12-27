c564f35653aa144dc3263c463fe811b68d2b26e2
hugoisrr <84rami@gmail.com>
2019-12-27 12:35:04 +0100

    refactor Search component with Hooks

--- src/components/drinks/Search.js
+++ src/components/drinks/Search.js
@@ -1,70 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-  state = {
-    text: ''
-  };
+const Search = ({ searchDrinks, showClear, clearDrinks, setAlert }) => {
+  const [text, setText] = useState('');
 
-  static propTypes = {
-    searchDrinks: PropTypes.func.isRequired,
-    clearDrinks: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter a drink', 'brown');
+    if (text === '') {
+      setAlert('Please enter a drink', 'brown');
     } else {
-      this.props.searchDrinks(this.state.text);
-      this.setState({ text: '' });
+      searchDrinks(text);
+      setText('');
     }
   };
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  const onChange = e => setText(e.target.value);
 
-  render() {
-    const { showClear, clearDrinks } = this.props;
-    return (
-      <div className="row">
-        <div className="col s12">
-          <form onSubmit={this.onSubmit}>
-            <div className="input-field col s12 l8">
-              <i className="material-icons prefix">search</i>
-              <input
-                id="search_drink"
-                type="text"
-                className="validate"
-                name="text"
-                value={this.state.text}
-                onChange={this.onChange}
-              />
-              <label htmlFor="search_drink">Search Drink</label>
-            </div>
-            <div className="input-field col s6 l2">
-              <button className="btn waves-effect waves-light" type="submit">
-                Search
-                <i className="material-icons right">send</i>
-              </button>
-            </div>
-          </form>
-          {showClear && (
-            <div className="input-field col s6 l2">
-              <button
-                className="btn waves-effect waves-light red"
-                onClick={clearDrinks}
-              >
-                Clear
-                <i className="material-icons right">clear</i>
-              </button>
-            </div>
-          )}
-        </div>
+  return (
+    <div className="row">
+      <div className="col s12">
+        <form onSubmit={onSubmit}>
+          <div className="input-field col s12 l8">
+            <i className="material-icons prefix">search</i>
+            <input
+              id="search_drink"
+              type="text"
+              className="validate"
+              name="text"
+              value={text}
+              onChange={onChange}
+            />
+            <label htmlFor="search_drink">Search Drink</label>
+          </div>
+          <div className="input-field col s6 l2">
+            <button className="btn waves-effect waves-light" type="submit">
+              Search
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
+        </form>
+        {showClear && (
+          <div className="input-field col s6 l2">
+            <button
+              className="btn waves-effect waves-light red"
+              onClick={clearDrinks}
+            >
+              Clear
+              <i className="material-icons right">clear</i>
+            </button>
+          </div>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchDrinks: PropTypes.func.isRequired,
+  clearDrinks: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 
 export default Search;
